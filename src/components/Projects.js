42a8698fd8b5210a10c98d4c3386a5fad6e659f6
kamiltlalka <56155489+kamiltlalka@users.noreply.github.com>
2023-11-09 15:41:30 +0100

    fill all projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,8 @@ import Game from '../image/UnityGame.png'
 import CNC from '../image/CNC.png'
 import elliteq from '../image/elliteq.jpg'
 import tiande from '../image/tiande.png'
-
+import asm from '../image/asm.png'
+import pixel from '../image/pixel.png'
 
 function Frame({content, image}){
     return(
@@ -35,9 +36,8 @@ export default function Projects() {
         <Frame content={"Software for internet droggery to count loyality points from purchases made by their clients. Aplicatin used simple file database with was most siutablke to this solution. Aplication was made in .Net technology for desktop devices"} image={tiande}/>
         <Frame content={"Game for mobile devices on Unity engine. The game logic was made in c#. The application used puilt in accelerometer for controling ingame object. The game also fetured in-game shop for purchasing cosmetic ubgrades"} image={Game}/>
         <Frame content={"One of my enginieering project was designing and manufacturing a CNC mashine using 3D printing techniques. The whole mashine was designed in Fusion 360 and then manufactured and assembled."} image={CNC}/>
-        <Frame content={"Something about merger"} image={Webpage}/>
-        <Frame content={"Something about 2 project"} image={Webpage}/>
-        
+        <Frame content={"Application to fuse graphics in to one using multiple procesor cores. The application is written in C# and has 2 externals libraries. One library is in C++ and second ASM. This program allows to compare speed of assembly language to C++ language"} image={asm}/>
+        <Frame content={"Build front end web page for Mobile Photokiosk. The website was made using Blazor technology, .Net and C#. The site was made during internship at Pixel-Tech"} image={pixel}/>
 
     </div>
 
