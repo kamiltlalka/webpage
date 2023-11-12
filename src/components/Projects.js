3bcea3a27f24bbe47097b9dc62f58c695448c306
kamiltlalka <56155489+kamiltlalka@users.noreply.github.com>
2023-11-12 19:42:11 +0100

    fix some grammar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,13 +31,13 @@ export default function Projects() {
      <div className="topsection">Previous projects</div>
     <div className="d-flex flex-row flex-wrap justify-content-around">
 
-        <Frame content={"This website is one of my projects. It was made using React.js with Bootstrap. The source code can be found on my github page, where the page is hosted"} image={Webpage}/>
-        <Frame content={"Working on developing new functionalities in our projects. Integration with external API's for example payment providers or certified signatures. I also worked on integration with payment terminals to our software."} image={elliteq}/>
-        <Frame content={"Software for internet droggery to count loyality points from purchases made by their clients. Aplicatin used simple file database with was most siutablke to this solution. Aplication was made in .Net technology for desktop devices"} image={tiande}/>
-        <Frame content={"Game for mobile devices on Unity engine. The game logic was made in c#. The application used puilt in accelerometer for controling ingame object. The game also fetured in-game shop for purchasing cosmetic ubgrades"} image={Game}/>
-        <Frame content={"One of my enginieering project was designing and manufacturing a CNC mashine using 3D printing techniques. The whole mashine was designed in Fusion 360 and then manufactured and assembled."} image={CNC}/>
-        <Frame content={"Application to fuse graphics in to one using multiple procesor cores. The application is written in C# and has 2 externals libraries. One library is in C++ and second ASM. This program allows to compare speed of assembly language to C++ language"} image={asm}/>
-        <Frame content={"Build front end web page for Mobile Photokiosk. The website was made using Blazor technology, .Net and C#. The site was made during internship at Pixel-Tech"} image={pixel}/>
+        <Frame content={"This website is one of my projects, created using React.js with Bootstrap. The source code can be found on my GitHub page, where the website is also hosted."} image={Webpage}/>
+        <Frame content={"I contributed to the development of new functionalities in our projects, including integration with external APIs such as payment providers and certified signatures. Additionally, I worked on the integration of payment terminals with our software."} image={elliteq}/>
+        <Frame content={"The software for an online cosmetics store was created to calculate loyalty points from client purchases. The application used a simple file database, which was considered the most suitable solution. It was developed using .NET technology for desktop devices."} image={tiande}/>
+        <Frame content={"The mobile game, developed on the Unity engine, had its game logic implemented in C#. The application utilized the built-in accelerometer for controlling in-game objects. Additionally, the game featured an in-game shop for purchasing cosmetic upgrades."} image={Game}/>
+        <Frame content={"One of my engineering projects involved designing and manufacturing a CNC machine using 3D printing techniques. The entire machine was designed in Fusion 360 and then fabricated and assembled."} image={CNC}/>
+        <Frame content={"I developed an application to merge graphics using multiple processor cores. The application, written in C#, incorporates two external libraries—one in C++ and the other in ASM. This program enables the comparison of the speed between assembly language and C++ language."} image={asm}/>
+        <Frame content={"I built the front-end web page for a Mobile Photokiosk using Blazor technology, .NET, and C#. This website was developed during my internship at Pixel-Tech."} image={pixel}/>
 
     </div>
 
