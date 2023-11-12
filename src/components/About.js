d75a32878bf53d0450c204a6da1f126eef733b06
kamiltlalka <56155489+kamiltlalka@users.noreply.github.com>
2023-11-12 23:10:59 +0100

    Update text

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default function About() {
      <div className="topsection">About me</div>
     <div className="d-flex flex-column align-self-center justify-content-around containerabout">
         <div> <div className="pinksq1">  Who am I? <p> I am software developer who worked on few applications in many 
-                                         technologies form html/css through blazor/c#, React.js, 
+                                         technologies from html/css through blazor/c#, React.js, 
                                          microcontrollers such as Arduino, and many more. I'm a fast learner, who has a lot of motivation to work on even the hardest projects.</p>  </div></div>
                                          
         <div > <div className="violsq1">  My education and expirience <p> I graduated Silesian Univesity of Technology with a degree of Bachelor of Science in Computer Science with specialization in software and computer graphics. 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,13 +31,13 @@ export default function Projects() {
      <div className="topsection">Previous projects</div>
     <div className="d-flex flex-row flex-wrap justify-content-around">
 
-        <Frame content={"This website is one of my projects, created using React.js with Bootstrap. The source code can be found on my GitHub page, where the website is also hosted."} image={Webpage}/>
+        <Frame content={"This website is one of my projects, created using React.js with Bootstrap. I used git as a vesrion control tool and put the source code on my github. The site is hosted using github pages. The page is still in development since I plan to include more projects, on which I'm still working."} image={Webpage}/>
         <Frame content={"I contributed to the development of new functionalities in our projects, including integration with external APIs such as payment providers and certified signatures. Additionally, I worked on the integration of payment terminals with our software."} image={elliteq}/>
         <Frame content={"The software for an online cosmetics store was created to calculate loyalty points from client purchases. The application used a simple file database, which was considered the most suitable solution. It was developed using .NET technology for desktop devices."} image={tiande}/>
         <Frame content={"The mobile game, developed on the Unity engine, had its game logic implemented in C#. The application utilized the built-in accelerometer for controlling in-game objects. Additionally, the game featured an in-game shop for purchasing cosmetic upgrades."} image={Game}/>
-        <Frame content={"One of my engineering projects involved designing and manufacturing a CNC machine using 3D printing techniques. The entire machine was designed in Fusion 360 and then fabricated and assembled."} image={CNC}/>
+        <Frame content={"One of my engineering projects involved designing and manufacturing a CNC machine using 3D printing techniques. The entire machine was designed in Fusion 360 and then fabricated and assembled. The control system is based on Arduino, which manages the motors responsible for moving the machine."} image={CNC}/>
         <Frame content={"I developed an application to merge graphics using multiple processor cores. The application, written in C#, incorporates two external libraries—one in C++ and the other in ASM. This program enables the comparison of the speed between assembly language and C++ language."} image={asm}/>
-        <Frame content={"I built the front-end web page for a Mobile Photokiosk using Blazor technology, .NET, and C#. This website was developed during my internship at Pixel-Tech."} image={pixel}/>
+        <Frame content={"I built the front-end web page for a Mobile Photokiosk using Blazor technology, .NET, and C#. Integrating with Google API to download images from Google was one of bigger parts of this project. This website was developed during my internship at Pixel-Tech."} image={pixel}/>
 
     </div>
 
