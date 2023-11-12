7961310bab15ee8243417458d6cb55b783ee3966
kamiltlalka <56155489+kamiltlalka@users.noreply.github.com>
2023-11-12 22:46:32 +0100

    update text

--- src/components/About.js
+++ src/components/About.js
@@ -11,10 +11,12 @@ export default function About() {
     <div className="d-flex flex-column align-self-center justify-content-around containerabout">
         <div> <div className="pinksq1">  Who am I? <p> I am software developer who worked on few applications in many 
                                          technologies form html/css through blazor/c#, React.js, 
-                                         microcontrollers such as Arduino, and many more. I learn very quick 
-                                         and have a lot of motivation to work.</p>  </div></div>
+                                         microcontrollers such as Arduino, and many more. I'm a fast learner, who has a lot of motivation to work on even the hardest projects.</p>  </div></div>
                                          
-        <div > <div className="violsq1">  My education and expirience <p> My name is kamil and i just graduated from university.</p>  </div></div>
+        <div > <div className="violsq1">  My education and expirience <p> I graduated Silesian Univesity of Technology with a degree of Bachelor of Science in Computer Science with specialization in software and computer graphics. 
+                                          I worked on different programming projects during course of my studies in C ,C++, C# and Java Script, Java and assembler. I gained my experience working as a commercial software developer for elliteq.
+          
+          </p>  </div></div>
     </div>
 
 
